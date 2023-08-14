e2a5d59c1828ede71591a82de7926b828899f56f
Pascal Wittler <git@pascalwittler.de>
2023-08-14 22:02:10 +0200

    add basic python flask setup

--- api/app.py
+++ api/app.py
@@ -1,4 +1,15 @@
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
+
 app = Flask(__name__)
+api = Api(app)
+
+CORS(app)
+
+@app.route('/')
+def index():
+    return {'message': 'Hello world!', 'data': {}}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2342, debug=True)
