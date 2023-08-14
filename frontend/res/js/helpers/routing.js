746782ebba749910398292e32e6b992a3d3428fb
Pascal Wittler <git@pascalwittler.de>
2023-08-15 01:18:12 +0200

    add valid url routes + actions to routing.js

--- frontend/res/js/helpers/routing.js
+++ frontend/res/js/helpers/routing.js
@@ -1,4 +1,23 @@
 export class Routing {
+  ROUTES = {
+    '/employees/': {
+      module: 'Employee',
+      action: 'list',
+    },
+    '/employee/add/': {
+      module: 'Employee',
+      action: 'add',
+    },
+    '/employee/edit/': {
+      module: 'Employee',
+      action: 'edit',
+    },
+    '/employee/delete/': {
+      module: 'Employee',
+      action: 'delete',
+    },
+  };
+
   constructor() {
     this.route = window.location.pathname.replace(/\/?$/, '/');
   }
