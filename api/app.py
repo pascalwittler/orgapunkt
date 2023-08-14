01a18199220d96ff7d83998f9a615ef7afe7e491
Pascal Wittler <git@pascalwittler.de>
2023-08-14 22:03:45 +0200

    add python shelve helper functions

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import shelve
+
 from flask import Flask, g
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
@@ -6,6 +8,18 @@ app = Flask(__name__)
 api = Api(app)
 
 CORS(app)
+
+def get_db(database_name):
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open(database_name)
+    return db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def index():
