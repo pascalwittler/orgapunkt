5439cb99d75d5877655d0d0ecbe9cd00f06f13ba
Pascal Wittler <git@pascalwittler.de>
2023-08-15 20:47:08 +0200

    add new constructor to rendering.js

--- frontend/res/js/helpers/rendering.js
+++ frontend/res/js/helpers/rendering.js
@@ -1,4 +1,9 @@
 export class Rendering {
+  constructor(module, action) {
+    this.module = module;
+    this.action = action;
+  }
+
   insertHtml(wrapper, htmlContent) {
     wrapper.innerHTML = htmlContent;
   }
