7cd46e5297cc9eec4247e141ebdb17b542520eb1
Pascal Wittler <git@pascalwittler.de>
2023-08-15 20:49:07 +0200

    add navigationjs module

--- frontend/res/js/main.js
+++ frontend/res/js/main.js
@@ -2,4 +2,5 @@ import { Routing } from './helpers/routing.js';
 import { Rendering } from './helpers/rendering.js';
 import { Api } from './helpers/api.js';
 
+import { Navigation } from './modules/navigation.js';
 import { Employee } from './modules/employee.js';
