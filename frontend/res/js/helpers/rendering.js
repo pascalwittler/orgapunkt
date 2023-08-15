fe84e3734e085a5d08edcfd68d79d07b914e9ee0
Pascal Wittler <git@pascalwittler.de>
2023-08-15 20:33:01 +0200

    add show + hide functions to rendering.js

--- frontend/res/js/helpers/rendering.js
+++ frontend/res/js/helpers/rendering.js
@@ -2,4 +2,12 @@ export class Rendering {
   insertHtml(wrapper, htmlContent) {
     wrapper.innerHTML = htmlContent;
   }
+
+  show(domElement) {
+    domElement.removeAttribute('hidden');
+  }
+
+  hide(domElement) {
+    domElement.setAttribute('hidden', '');
+  }
 }
