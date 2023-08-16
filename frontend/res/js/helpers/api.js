62832f1d118dcb92697bb74b3536137607669784
Pascal Wittler <git@pascalwittler.de>
2023-08-16 21:29:06 +0200

    add sendRequest() function to api.js

--- frontend/res/js/helpers/api.js
+++ frontend/res/js/helpers/api.js
@@ -2,4 +2,19 @@ export class Api {
   constructor() {
     this.baseUrl = 'http://localhost:7892';
   }
+
+  async sendRequest(route, data = null, method = 'GET') {
+    const response = await fetch(
+      this.baseUrl + route,
+      {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: (data ? JSON.stringify(data) : null)
+      }
+    );
+
+    return response.json();
+  }
 }
