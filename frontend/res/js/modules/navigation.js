c7bf24df7aa403d39f0f599c09cf191f7fc5c644
Pascal Wittler <git@pascalwittler.de>
2023-08-17 20:37:38 +0200

    sort employee data by initials

--- frontend/res/js/modules/navigation.js
+++ frontend/res/js/modules/navigation.js
@@ -5,7 +5,7 @@ export class Navigation {
   async navigate() {
     const api = new Api();
     const response = await api.sendRequest('/employees');
-    const employees = response.data;
+    const employees = (response.data).sort((a, b) => a.identifier.localeCompare(b.identifier));
 
     const sectionDomElement = document.querySelector('section[data-module="navigation"][data-action="navigate"]');
     const employeesListDomElement = sectionDomElement.querySelector('ul.employees');
