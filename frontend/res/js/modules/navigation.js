e8f0ce23cb3d2dd46a79461f5fe00fd13c22aeab
Pascal Wittler <git@pascalwittler.de>
2023-08-17 18:50:30 +0200

    add api query to navigation navigate action

--- frontend/res/js/modules/navigation.js
+++ frontend/res/js/modules/navigation.js
@@ -2,6 +2,12 @@ import { Api } from '../helpers/api.js';
 
 export class Navigation {
   async navigate() {
+    const api = new Api();
+    const response = await api.sendRequest('/employees');
+    const employees = response.data;
+
+    const sectionDomElement = document.querySelector('section[data-module="navigation"][data-action="navigate"]');
+    const employeesListDomElement = sectionDomElement.querySelector('ul.employees');
 
   }
 }
