12bad536c09d9ec3fc554058b5cdd0b70ab127eb
Pascal Wittler <git@pascalwittler.de>
2023-08-17 18:50:51 +0200

    render api result in navigation navigate action

--- frontend/res/js/modules/navigation.js
+++ frontend/res/js/modules/navigation.js
@@ -1,4 +1,5 @@
 import { Api } from '../helpers/api.js';
+import { Rendering } from '../helpers/rendering.js';
 
 export class Navigation {
   async navigate() {
@@ -9,5 +10,20 @@ export class Navigation {
     const sectionDomElement = document.querySelector('section[data-module="navigation"][data-action="navigate"]');
     const employeesListDomElement = sectionDomElement.querySelector('ul.employees');
 
+    Rendering.clearHtml(employeesListDomElement);
+
+    employees.forEach((employee) => {
+      Rendering.insertHtml(employeesListDomElement, `
+        <li class="employee">
+          <span class="initials">${employee.identifier.toUpperCase()}</span>
+          <span class="name">${employee.firstName} ${employee.lastName}</span>
+          <span class="email">${employee.email}</span>
+          <span class="controls">
+            <a href="/employee/${employee.identifier.toLowerCase()}/edit/" class="edit">Edit</a>
+            <a href="/employee/${employee.identifier.toLowerCase()}/delete/" class="remove">Delete</a>
+          </span>
+        </li>
+      `);
+    });
   }
 }
